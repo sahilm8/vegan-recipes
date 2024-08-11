b2d28be7e3d3791d0c05224932a3ab01954d3592
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-11 19:44:59 +0100

    Style results page

--- src/pages/results/Results.tsx
+++ src/pages/results/Results.tsx
@@ -1,8 +1,52 @@
-import React from "react"
-import "./notFound.css"
+import React, { useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import "./results.css"
+import { getRecipes } from "../../utils/api"
 
 const Results: React.FC = () => {
-  return <div>Results</div>
+  const navigate = useNavigate()
+  const { query } = useParams<{ query: string }>()
+  const [results, setResults] = React.useState<any[]>([])
+
+  useEffect(() => {
+    getRecipes(query!, 0, 50).then((data) => {
+      setResults(data)
+    })
+  }, [query])
+
+  return (
+    <div className="results-root">
+      <div className="results-header">
+        <p className="results-heading" onClick={() => navigate("/")}>
+          Vegan Recipes
+        </p>
+        <input
+          type="text"
+          placeholder="Search for recipes"
+          value={""}
+          onChange={(e) => {}}
+          className="results-search-bar"
+          onKeyDown={() => {}}
+        />
+        <button className="results-search-button" onClick={() => {}}>
+          Search
+        </button>
+      </div>
+      <div className="results-list">
+        {Array.isArray(results) && results.length !== 0 ? (
+          results.map((result) => (
+            <div className="results-row">
+              <p className="results-recipe-title">{result.title}</p>
+            </div>
+          ))
+        ) : (
+          <p className="results-no-results">
+            No recipes found, please try again later.
+          </p>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Results
