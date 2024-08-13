56dfd66df2f34bf562fd30523c4b470312bcc663
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-13 16:31:12 +0100

    Update recipe and its styling

--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -1,35 +1,42 @@
 import React, { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { getRecipeData } from "../../utils/api"
 import "./recipe.css"
 
 const Recipe: React.FC = () => {
+  const navigate = useNavigate()
   const { uri } = useParams<{ uri: string }>()
-  const [recipe, setRecipe] = useState<any>({
-    label: "123",
-    image: "123",
-    cuisineType: "123",
-    dishType: "123",
-    mealType: "123",
-  })
-
+  const [recipe, setRecipe] = useState<any>({})
 
   useEffect(() => {
-      getRecipeData(uri!)
-        .then((data) => {
-          setRecipe(data.recipe)
-        })
-        .catch((error) => alert(error))
+    getRecipeData(uri!)
+      .then((data) => {
+        setRecipe(data.recipe)
+      })
+      .catch((error) => alert(error))
   }, [uri])
 
   return (
     <div className="recipe-root">
       <div className="recipe-header">
-        <p className="recipe-heading">{recipe.label}</p>
+        <p className="recipe-heading" onClick={() => navigate("/")}>
+          Vegan Recipes
+        </p>
+        <p className="recipe-sub-heading">{recipe.label}</p>
         <img src={recipe.image} alt={recipe.label} className="recipe-image" />
-        <p className="recipe-sub-heading">{recipe.cuisineType}</p>
-        <p className="recipe-sub-heading">{recipe.dishType}</p>
-        <p className="recipe-sub-heading">{recipe.mealType}</p>
+        <p className="recipe-section-heading">Ingredients</p>
+        {recipe.ingredients.map((ingredient: any) => (
+          <div className="recipe-ingredient-container">
+            <img
+              src={ingredient.image}
+              alt={ingredient.text}
+              className="recipe-ingredient-image"
+            />
+            <p className="recipe-ingredient" key={ingredient.text}>
+              {ingredient.text}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   )
