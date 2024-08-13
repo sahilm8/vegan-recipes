e5fc8e69507102ae2772839e81befb8b0a3bc9d5
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-13 04:02:20 +0100

    Add getRecipeData, update endpoints

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,11 +5,12 @@ export const getRecipes = async (
   from: number,
   to: number,
 ): Promise<any> => {
-  const url = `${process.env.REACT_APP_EDAMAM_BASE_URL}/search`
+  const url = `${process.env.REACT_APP_EDAMAM_BASE_URL}/api/recipes/v2`
   const params = {
     q: query,
     app_id: process.env.REACT_APP_EDAMAM_APP_ID,
     app_key: process.env.REACT_APP_EDAMAM_APP_KEY,
+    type: "public",
     health: "vegan",
     from: from,
     to: to,
@@ -26,6 +27,21 @@ export const getRecipes = async (
   }
 }
 
-export const getRecipeData = async (id: string): Promise<any> => {
-  const url = `${process.env.REACT_APP_EDAMAM_BASE_URL}/api/recipes/v2/${id}`
+export const getRecipeData = async (uri: string): Promise<any> => {
+  const url = `${process.env.REACT_APP_EDAMAM_BASE_URL}/api/recipes/v2/${uri}`
+  const params = {
+    app_id: process.env.REACT_APP_EDAMAM_APP_ID,
+    app_key: process.env.REACT_APP_EDAMAM_APP_KEY,
+    type: "public",
+  }
+  try {
+    const response = await axios.get(url, { params })
+    return response.data
+  }
+  catch (error) {
+    if (error instanceof Error) {
+      return { error: error.message }
+    }
+    return { error: "An unknown error occurred" }
+  }
 }
