79c19ee91b9c0b260b7f673c7d1e6bece92c6494
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-13 04:01:35 +0100

    Add recipe in app, apply basic styling to recipe, fetch recipe data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/Home"
 import Results from "./pages/results/Results"
+import Recipe from "./pages/recipe/Recipe"
 import NotFound from "./pages/notFound/NotFound"
 
 const App: React.FC = () => {
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/results/:query" element={<Results />} />
+        <Route path="/recipe/:uri" element={<Recipe />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-//import { getRecipeData } from "../../utils/api"
+import { getRecipeData } from "../../utils/api"
 import "./recipe.css"
 
 const Recipe: React.FC = () => {
-  const { id } = useParams<{ id: string }>()
-  const [recipe, setRecipe] = useState<any>({})
+  const { uri } = useParams<{ uri: string }>()
+  const [recipe, setRecipe] = useState<any>({
+    label: "123",
+    image: "123",
+    cuisineType: "123",
+    dishType: "123",
+    mealType: "123",
+  })
 
-  /*
+
   useEffect(() => {
-    if (id) {
-      getRecipeData(id)
+      getRecipeData(uri!)
         .then((data) => {
           setRecipe(data.recipe)
         })
         .catch((error) => alert(error))
-    }
-  }, [id])
-    */
+  }, [uri])
 
   return (
     <div className="recipe-root">
@@ -27,7 +30,6 @@ const Recipe: React.FC = () => {
         <p className="recipe-sub-heading">{recipe.cuisineType}</p>
         <p className="recipe-sub-heading">{recipe.dishType}</p>
         <p className="recipe-sub-heading">{recipe.mealType}</p>
-        <p className="recipe-sub-heading">{recipe.dishType}</p>
       </div>
     </div>
   )
